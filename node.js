bf767144e3309ceb39de9715f591d1b792f2480a
Sourish2023 <153942118+Sourish2023@users.noreply.github.com>
2023-12-31 15:20:47 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,41 @@
+let age = 13;
+(age>13)?console.log("you can drive"):console.log("you cannot drive")
+let url=home;
+switch(url){
+    case "home":
+    console.log("home page")
+    break;
+    case"about":
+console.log("about page")
+break;
+case"contact":
+console.log("contact page")
+}
+function display(val){
+    document.getElementById("result").value +- val;
+}function solve(){
+    let x= document.getElementById("result").value
+    let y=eval(x);
+    document.getElementById("result").value=y
+}
+function clearScreen(){
+    document.getElementById('result').value='index.html'
+a.adjacentHTML(below).p
+}
+{
+    link.href="https://web.whatsapp.com/";
+    console.log("home and about us")
+    getElementById('print').we
+}
+div("just a second")
+{
+    getElementById
+    sourish.hidden="true";
+}
+clickbtn.onclick.getElementById('click')
+Function().link.href="https://submit.jotform.com/2325119432614"
+let input=document.getElementById('input')
+function btn(event){
+    console.log(event.target.value);
+}
+input.addEventListener('click',btn)
